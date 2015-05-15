83a085da874d758194c779ef84feecb428a93233
xiaorong1176 <750106615@qq.com>
2015-05-15 10:16:41 +0800

    Add FaceppDetect codes.

--- src/com/echo/how_old/FaceppDetect.java
+++ src/com/echo/how_old/FaceppDetect.java
@@ -1,11 +1,15 @@
 package com.echo.how_old;
 
+import java.io.ByteArrayOutputStream;
+
 import org.json.JSONObject;
 
 import com.facepp.error.FaceppParseException;
 import com.facepp.http.HttpRequests;
+import com.facepp.http.PostParameters;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
 public class FaceppDetect {
 	
@@ -16,14 +20,38 @@ public class FaceppDetect {
 		void error(FaceppParseException exception);
 	}
 	
-	public static void detect(Bitmap bm, final CallBack callBack){
+	public static void detect(final Bitmap bm, final CallBack callBack){
 		//耗时操作 用了匿名内部类，最好使用final
 		new Thread(new Runnable() {
 			
 			public void run() {
-				// request
-				HttpRequests requests = new HttpRequests(Constant.KEY, Constant.SECRET, true, true);
-				//将bitmap转换为二进制的字节数组
+				try {
+					// request
+					HttpRequests requests = new HttpRequests(Constant.KEY, Constant.SECRET, true, true);
+					//将bitmap转换为二进制的字节数组
+					Bitmap bmSmall = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight());
+					ByteArrayOutputStream stream = new ByteArrayOutputStream();
+					bmSmall.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+					
+					byte[] arrays = stream.toByteArray();
+					
+					PostParameters params = new PostParameters();
+					params.setImg(arrays);
+					JSONObject jsonObject = requests.detectionDetect(params);
+					
+					Log.e("Tag", jsonObject.toString());
+					
+					if(callBack != null){
+						callBack.success(jsonObject);
+					}
+				} catch (FaceppParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					if (callBack != null) {
+						callBack.error(e);
+					}
+				}
+				
 			}
 		}).start();
 		
